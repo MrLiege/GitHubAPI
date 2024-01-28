29b0d1c9665414b0c98e6a2c7cf6de2e12853c48
MrLiege <mister.4100@mail.ru>
2024-01-28 14:16:35 +0400

    search screen

---
+++ GitHubPet/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GitHubPet
+//
+//  Created by Artyom on 28.01.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
